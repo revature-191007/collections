110261141032487a831fcb2c5f743c71b27c61b6
Mitch Goshorn <msgoshorn@gmail.com>
2019-10-10 11:11:34 -0400

    Added collections notes

---
+++ src/main/java/com/revature/collections/CollectionsExplanation.java
@@ -0,0 +1,31 @@
+package com.revature.collections;
+
+/**
+ * Collections are Java's built in data structures.
+ * Collections are organized into a few different categories.
+ * 
+ * The primary collections start with the Iterable interface,
+ * which is extended by the Collection interface, which
+ * is then extended by several sub interfaces that represent
+ * abstract data structures.
+ * 
+ * 1. Set - Sets contain distinct items. Very optimal in situations
+ * 			where we will need to frequently or quickly determine if some
+ * 			object is contained in the set.
+ * 		Implementations:
+ * 			* HashSet
+ * 
+ * 2. List - An ordered list of items. Duplicates are valid in a list.
+ * 		Implementations:
+ * 			* ArrayList
+ * 			* LinkedList
+ * 
+ * 3. Queue - FIFO (first in, first out) data structure. Items added at the end
+ * 			and removed from the beginning.
+ *		Implementations:
+ *			* LinkedList
+ *			* PriorityQueue
+ */
+public class CollectionsExplanation {
+
+}
